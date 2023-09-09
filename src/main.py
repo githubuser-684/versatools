1560b64496a8816ea45799531be866502bce8350
Garryybd <118138251+GarryyBD@users.noreply.github.com>
2023-09-09 14:37:14 -0400

    kill program after CTRL+C

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 from App import App
 from multiprocessing import freeze_support
+import signal
+import os
+
+def signal_handler(sig, frame):
+    print('\nBye!')
+    os.kill(os.getpid(), 9)
 
 if __name__ == "__main__":
     freeze_support() # needed for multiprocessing on windows
+    signal.signal(signal.SIGINT, signal_handler)
+
     app = App()
     app.run()

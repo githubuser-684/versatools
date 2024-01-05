3495f795d2b0113d93c28a452065a1bad6139d0e
Garryybd <118138251+GarryyBD@users.noreply.github.com>
2024-01-05 01:31:25 -0500

    update run.bat

--- run.bat
+++ run.bat
@@ -1,13 +1,14 @@
 @echo off
 
 echo Loading Versatools...
-git reset --hard > nul 2>&1
 git pull --all > nul 2>&1
 if not exist .venv python -m venv .venv
 call .venv\Scripts\activate
 pip install -r requirements.txt > nul 2>&1
 cls
 doskey versatools=python src\main.py
+
 echo Use 'versatools' to run Versatools.
+echo Made by garryybd#0
 
 cmd

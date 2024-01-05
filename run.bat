e13ffa057b4d2cf194aa630e2e27fbe808fab4f3
Garryybd <118138251+GarryyBD@users.noreply.github.com>
2024-01-05 12:55:57 -0500

    fix run.bat

--- run.bat
+++ run.bat
@@ -6,7 +6,7 @@ if not exist .venv python -m venv .venv
 call .venv\Scripts\activate
 pip install -r requirements.txt > nul 2>&1
 cls
-doskey versatools=python src\main.py
+doskey versatools=python src\main.py $*
 
 echo Use 'versatools' to run Versatools.
 echo Made by garryybd#0
